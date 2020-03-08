77e04d360852e3f984eca63479e9fe771d1f84cf
qbolec <qbolec@gmail.com>
2020-03-08 17:45:28 +0100

    wiecej rodzajow wody

--- index.js
+++ index.js
@@ -443,7 +443,7 @@ const zasadyPotwora = [
     po:['o'],
   },
 ];
-const wody=[1,2,3,4,5];
+const wody=[1,2,3,4,5,6,7,8,9];
 const max_wody=wody[wody.length-1];
 const zasadySpadaniaWody=[
   ...concat(wody.map(g=>[
